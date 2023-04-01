c037cff871a8e7155944a015d8a0d0f8dd1ca95f
narmuni123 <narmuni123@gmail.com>
2023-04-01 17:34:21 +0530

    Added callback

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -10,9 +10,11 @@ class AuthForm extends StatefulWidget {
 class _AuthFormState extends State<AuthForm> {
   final _formKey = GlobalKey<FormState>();
 
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _userNameController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  var _isLogin = true;
+
+  String _email = "";
+  String _userName = "";
+  String _password = "";
 
   _trySubmit() {
     final isValid = _formKey.currentState!.validate();
@@ -26,62 +28,80 @@ class _AuthFormState extends State<AuthForm> {
   Widget build(BuildContext context) {
     return Center(
       child: Card(
-        margin: EdgeInsets.all(20),
+        margin: const EdgeInsets.all(20),
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
             child: Form(
               key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
-                    controller: _emailController,
+                    key: const ValueKey('email'),
                     validator: (value) {
                       if (value!.isEmpty || !value.contains('@')) {
                         return "Please enter a valid address";
                       }
                       return null;
                     },
+                    onSaved: (value) {
+                      _email = value!;
+                    },
                     keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       labelText: 'Email Address',
                     ),
                   ),
+                  !_isLogin
+                      ? TextFormField(
+                          key: const ValueKey('userName'),
+                          validator: (value) {
+                            if (value!.isEmpty || value.length < 4) {
+                              return "Please enter at least 4 characters.";
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _userName = value!;
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Username',
+                          ),
+                        )
+                      : const SizedBox(),
                   TextFormField(
-                    controller: _userNameController,
-                    validator: (value) {
-                      if (value!.isEmpty || value.length < 4) {
-                        return "Please enter at least 4 characters.";
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: _passwordController,
+                    key: const ValueKey('password'),
                     validator: (value) {
                       if (value!.isEmpty || value.length < 7) {
                         return "Password must be at least 7 characters long";
                       }
                       return null;
                     },
-                    decoration: InputDecoration(
+                    onSaved: (value) {
+                      _password = value!;
+                    },
+                    decoration: const InputDecoration(
                       labelText: 'Password',
                     ),
                     obscureText: true,
                   ),
-                  SizedBox(height: 12),
+                  const SizedBox(height: 12),
                   ElevatedButton(
                     onPressed: () {
                       _trySubmit();
                     },
-                    child: Text('Login'),
+                    child: Text(_isLogin ? 'Login' : 'Signup'),
                   ),
                   TextButton(
-                    child: Text('Create new account'),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    child: Text(_isLogin
+                        ? 'Create new account'
+                        : 'I already have an account'),
                   ),
                 ],
               ),
