f5ad9e19282deed4e01a85f2b8ff488b813e65d0
narmuni123 <narmuni123@gmail.com>
2023-04-03 20:11:42 +0530

    Added app icons

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Chat app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         backgroundColor: Colors.blueAccent,
