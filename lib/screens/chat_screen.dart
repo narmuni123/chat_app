aed4e1e66565ead11fa51a2b6a19f8b030794c4c
narmuni123 <narmuni123@gmail.com>
2023-04-11 11:48:05 +0530

    Added messages to chat scren

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/chat/messages.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -50,29 +51,14 @@ class ChatScreen extends StatelessWidget {
             });
           },
         ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection("chats/NutjHjwVS1N05L03eT4u/messages")
-              .snapshots(),
-          builder: (ctx, AsyncSnapshot<dynamic> streamSnapshot) {
-            if (streamSnapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final documents = streamSnapshot.data.docs;
-            return ListView.builder(
-              itemCount: documents.length,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (_, int index) {
-                return Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Text(documents[index]['text']),
-                );
-              },
-            );
-          },
+        body: SizedBox(
+          child: Column(
+            children: [
+              Expanded(
+                child: Messages(),
+              ),
+            ],
+          ),
         ));
   }
 }
